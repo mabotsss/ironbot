a299adf1fce7e0ad1a0295af085752c352655b5f
mabotsss <nandoscrz@gmail.com>
2021-02-28 16:16:51 +0700

    addedFilter

--- ironbot/modules/filter.py
+++ ironbot/modules/filter.py
@@ -1,7 +1,7 @@
 from asyncio import sleep
 from re import search, IGNORECASE, escape
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
+from ironbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from ironbot.events import register
 
 
 @register(incoming=True, disable_edited=True, disable_errors=True)
