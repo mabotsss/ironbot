55ec97f0f4f3899bb55c58b33bb4fec406a2fcf8
mabotsss <nandoscrz@gmail.com>
2021-03-05 09:24:28 +0700

    .

--- ironbot/modules/phreaker.py
+++ ironbot/modules/phreaker.py
@@ -13,186 +13,32 @@ from telethon import events
 import asyncio
 from ironbot.cmdhelp import CmdHelp
 
-
-
-@register(outgoing=True, pattern=r"^\.device(?: |$)(\S*)")
+@register(outgoing=True, pattern=r"^\.cok(?: |$)(\S*)")
 async def device_info(request):
     textx = await request.get_reply_message()
-    codename = request.pattern_match.group(1)
-    if codename:
-        pass
-    elif textx:
-        codename = textx.text
-    else:
-        await request.edit("`Usage: .device <codename> / <model>`")
-        return
-    data = json.loads(
-        get(
-            "https://raw.githubusercontent.com/androidtrackers/"
-            "certified-android-devices/master/by_device.json"
-        ).text
-    )
-    results = data.get(codename)
-    if results:
-        reply = f"**Search results for {codename}**:\n\n"
-        for item in results:
-            reply += (
-                f"**Brand**: {item['brand']}\n"
-                f"**Name**: {item['name']}\n"
-                f"**Model**: {item['model']}\n\n"
-            )
-    else:
-        reply = f"`Couldn't find info about {codename}!`\n"
-    await request.edit(reply)
-
-@register(outgoing=True, pattern=r"^\.ngens(?: |$)(\S*)")
-async def device_info(request): 
-    textx = await request.get_reply_message()
     Query = request.pattern_match.group(1)
     if Query:
         pass
     elif textx:
         Query = textx.text
     else:
-        await request.edit("`Usage: .device <Query> `")
+        await request.edit("`Usage: .cok <Query>`")
         return
-    data = json.loads(
-         get(
-             "http://ip-api.com/json/{Query}/"
-         ).text
-    )
-    results = data.get(Query)
-    if results:
-         reply = (f"**Latest TWRP for {Query}:**\n")
-         for item in results:
-             reply += (
-                 f"**Brand**: {item['query']}\n"
-                 f"**Name**: {item['status']}\n"
-             )       
-    else:
-        reply = f"`Couldn't find info about {Query}!`\n"
-    await request.edit(reply)
-    
-    
-@register(outgoing=True, pattern=r"^\.cok(?: |$)(\S*)")
-async def device_info(request): 
-    textx = await request.get_reply_message()
-    Query = request.pattern_match.group(1)
-    if Query:
-        pass
-    elif textx:
-        Query = textx.text
-    else:
-        await request.edit("`Usage: .device <Query> `")
+
+    url = f'http://ip-api.com/json/{Query}'
+    request = get(url)
+    result = json.loads(request.text)
+
+    if request.status_code != 200:
+        await weather.edit("`Invalid country.`")
         return
-    magisk_dict = {
-        "Tes": "http://ip-api.com/json/{Query}/"
-    }
-    releases = "Latest Magisk Releases:\n"
-    for k, release_url in magisk_dict.items():
-         data = get(release_url).json()
-         releases += (
-             f' ZIP v{data["query"]}')
-    await request.edit(releases)
 
+    qry = result['query']
+    stts = result['status']
+      
+    await weather.edit(
+        f"`{qry}, {stts}`\n")
 
-
-@register(outgoing=True, pattern=r"^\.twrp(?: |$)(\S*)")
-async def twrp(request):
-    textx = await request.get_reply_message()
-    device = request.pattern_match.group(1)
-    if device:
-        pass
-    elif textx:
-        device = textx.text.split(" ")[0]
-    else:
-        await request.edit("`Usage: .twrp <codename>`")
-        return
-    url = get(f"https://dl.twrp.me/{device}/")
-    if url.status_code == 404:
-        reply = f"`Couldn't find twrp downloads for {device}!`\n"
-        await request.edit(reply)
-        return
-    page = BeautifulSoup(url.content, "lxml")
-    download = page.find("table").find("tr").find("a")
-    dl_link = f"https://dl.twrp.me{download['href']}"
-    dl_file = download.text
-    size = page.find("span", {"class": "filesize"}).text
-    date = page.find("em").text.strip()
-    reply = (
-        f"**Latest TWRP for {device}:**\n"
-        f"[{dl_file}]({dl_link}) - __{size}__\n"
-        f"**Updated:** __{date}__\n"
-    )
-    await request.edit(reply)
-
-@register(outgoing=True, pattern=r"^\.magisk$")
-async def magisk(request):
-    magisk_dict = {
-        "Stable": "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json",
-        "Beta": "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/beta.json",
-    }
-    releases = "Latest Magisk Releases:\n"
-    for name, release_url in magisk_dict.items():
-        data = get(release_url).json()
-        releases += (
-            f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
-            f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | '
-            f'[Uninstaller]({data["uninstaller"]["link"]})\n')
-    await request.edit(releases)
-
-
-
-@register(outgoing=True, pattern="^.ipscan$")
-async def _(event):
-    textx = await request.get_reply_message()
-    input_str = request.pattern_match.group(1)
-    if codename:
-        pass
-    elif textx:
-        input_str = textx.text
-    else:
-        await request.edit("`Usage: .device <input_str> / <model>`")
-        return
-    adress = input_str    
-    token = "19e7f2b6fe27deb566140aae134dec6b"
-    api = "http://api.ipstack.com/" + adress + "?access_key=" + token + "&format=1"
-    
-    result = urllib.request.urlopen(api).read()
-    result = result.decode()
-
-    result = json.loads(result)
-    a = result["type"]
-    b = result["country_code"]
-    c = result["region_name"]
-    d = result["city"]
-    e = result["zip"]
-    f = result["latitude"]
-    g = result["longitude"]
-    await event.edit(
-        f"<b><u>Gathered Information</b></u>\n\n<b>Ip type :-</b><code>{a}</code>\n<b>Country code:- </b> <code>{b}</code>\n<b>State name :-</b><code>{c}</code>\n<b>City name :- </b><code>{d}</code>\n<b>zip :-</b><code>{e}</code>\n<b>Latitude:- </b> <code>{f}</code>\n<b>Longitude :- </b><code>{g}</code>\n",
-        parse_mode="HTML",
-    )
-    
-    
-@register(outgoing=True, pattern="^.host2ip$")
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(2)
-    Query = input_str
-    
-    api = "http://ip-api.com/json/" + Query
-    
-    result = urllib.request.urlopen(api).read()
-    result = result.decode()
-
-    result = json.loads(result)
-    a = result["query"]
-    await event.edit(
-        f"<b><u>Gathered Information</b></u>\n\n<b>Ip type :-</b><code>{a}</code>\n",
-        parse_mode="HTML",
-    )
 
 
 CMD_HELP.update(
